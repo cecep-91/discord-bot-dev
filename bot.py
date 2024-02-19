2e51a86c30e74f546737bbcc1faab3a8a8eaf95c
Ikuba Ru <cecep-91>
2024-02-19 14:45:29 +0700

    changes

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def hi(ctx):
     await ctx.send(f'Hello {ctx.author.mention}!')
 
 @bot.command()
-async def p(ctx):
+async def ping(ctx):
     await ctx.send(f'yeah, it works!')
 
 @bot.command()
