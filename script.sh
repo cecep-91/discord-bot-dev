9d6226c0c5a8a6231693e19ab457a846f5392949
Ikuba Ru <ikuu@8.8.8.8>
2024-03-01 17:52:07 +0700

    Change the schema of Kubernetes deployment and CI/CD

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bot=$(python -c "import base64; print(base64.b64encode(open('bot.py', 'rb').read()).decode())")
-req=$(python -c "import base64; print(base64.b64encode(open('requirements.txt', 'rb').read()).decode())")
-
-sed -i "s/bot.py:.*/bot.py: $bot/g" kubernetes/bot/discord-bot/bot-cm.yaml
-sed -i "s/requirements.txt:.*/requirements.txt: $req/g" kubernetes/bot/discord-bot/bot-cm.yaml
